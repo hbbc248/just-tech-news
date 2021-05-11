423f29e2c2426054734a9851be5241dd740e3f99
Ibrahim <ibrahimzerlin@hotmail.com>
2021-05-10 20:23:00 -0500

    homepage done

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+const homeRoutes = require('./home-routes.js');
+
 // midware
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 // This is so if we make a request to any endpoint that doesn't exist, we'll receive a 404 error indicating we have requested an incorrect resource, another RESTful API practice.
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+// to make style sheet available to the client
+const path = require('path');
+
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// set up handlebars as app template engine of choice
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// to add style sheet and serve as asset
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
